b2bdfdc1ecaff8feba66b83a914af3d2318c7c74
pasierb <michal.pasierbski@sooftly.com>
2013-08-03 12:11:52 +0200

    minore refactor

--- js/binary-clock.js
+++ js/binary-clock.js
@@ -1,8 +1,11 @@
 var BinaryClock = function (container, options) {
   this.container = container;
+  this.options = (options || {});
+  this.options.sections = (this.options.sections || ["hours","minutes","seconds"])
 
   this.create();
   this.start();
+
   return this;
 }
 
@@ -20,23 +23,16 @@ BinaryClock.prototype.stop = function () {
 
 BinaryClock.prototype.onTick = function () {
   var date = new Date();
-  var sections = ["hours","minutes","seconds"];
   var sectionsElements = this.container.childNodes;
   var section, sectionValue, sectionElementItems, binary;
 
-  for (var i=0; i<sections.length; i++) {
-    section = sections[i];
+  for (var i=0; i<this.options.sections.length; i++) {
+    section = this.options.sections[i];
     sectionValue = date["get"+section.charAt(0).toUpperCase()+section.slice(1)]();
 
     for (var j=0; j<2; j++) {
       sectionElementItems = sectionsElements[(i*2)+j].childNodes
-      if (j == 0) {
-        //set tens
-        binary = this.toBinary(parseInt(sectionValue/10),4);
-      } else {
-        //set unuits
-        binary = this.toBinary((sectionValue%10),4);
-      }
+      binary = this.toBinary((j == 0 ? parseInt(sectionValue/10) : (sectionValue%10)),4);
       for (var k=0; k<4; k++) {
         sectionElementItems[k].className = (binary[k] === "1" ? "set" : "");
       }
@@ -56,17 +52,16 @@ BinaryClock.prototype.toBinary = function (number, precision) {
 }
 
 BinaryClock.prototype.create = function () {
-  var sections = ["hours","minutes","seconds"];
   var subsections = ["tens","units"];
   var wrapper = document.createElement("div");
   var section, sectionName, sectionItem;
 
   this.container.className += " binary-clock";
 
-  for (var i=0; i<sections.length; i++) {
+  for (var i=0; i<this.options.sections.length; i++) {
     for (var j=0; j<subsections.length; j++) {
       section = document.createElement("div");
-      sectionName = sections[i]+"-"+subsections[j];
+      sectionName = this.options.sections[i]+"-"+subsections[j];
       section.className = sectionName;
       for (var k=0; k<4; k++) {
         sectionItem = document.createElement("div");
